a4037cd273515ce64461af955e73f8768c12ad93
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2026-05-05 23:06:55 -0600

    Update AppConstants.kt ruta de mi wifi

--- app/src/main/java/com/moviles/unaplanner/core/AppConstants.kt
+++ app/src/main/java/com/moviles/unaplanner/core/AppConstants.kt
@@ -6,7 +6,8 @@ object AppConstants {
         //const val BASE_URL = "http://10.0.2.2:5147/"
         // adriana
         val BASE_URL = "http://192.168.1.6:5232/"
-
+        //KEYNA
+        //val BASE_URL = "http://192.168.100.128:5232/"
         object Paths {
             const val AUTH_LOGIN = "api/auth/login"
         }
