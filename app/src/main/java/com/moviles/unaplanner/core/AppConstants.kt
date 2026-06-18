dc71d5624a3f2aeb50a0aeb1ee797a52c539b5ca
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-06-17 19:43:20 -0600

    Add other API for Admin

--- app/src/main/java/com/moviles/unaplanner/core/AppConstants.kt
+++ app/src/main/java/com/moviles/unaplanner/core/AppConstants.kt
@@ -13,8 +13,10 @@ object AppConstants {
         //Adriana Usando IP local
         //private const val PHYSICAL_DEVICE_URL = "http://192.168.1.6:5232/"
         //REICHEL Usando IP local
-        private const val PHYSICAL_DEVICE_URL = "http://192.168.100.163:5232/"
+        //private const val PHYSICAL_DEVICE_URL = "http://192.168.100.163:5232/"
         //private const val PHYSICAL_DEVICE_URL = "http://192.168.100.64:5232/"
+        //private const val PHYSICAL_DEVICE_URL = "http://192.168.100.124:5232/"
+        //private const val PHYSICAL_DEVICE_URL = "http://192.168.0.13:5232/"
         //Natalia Usando IP local
 
         /**
@@ -54,6 +56,7 @@ object AppConstants {
             const val USER_PROFILE = "api/profile/{id}"
             const val CHANGE_PASSWORD = "api/profile/{id}/change-password"
             const val CAREERS = "api/careers"
+            const val ADMIN_CAREERS = "api/admin/careers"
             const val CAREER_CURRICULUM = "api/careers/{id}/curriculum"
             const val STUDENT_CURRICULUM_COURSES = "api/student/{id}/curriculum/courses"
             const val STUDENT_COURSE_PROGRESS = "api/student/{id}/courses/{courseId}"
--- app/src/main/java/com/moviles/unaplanner/data/remote/ApiService.kt
+++ app/src/main/java/com/moviles/unaplanner/data/remote/ApiService.kt
@@ -18,7 +18,7 @@ interface ApiService {
     @POST(AppConstants.Api.Paths.CHANGE_PASSWORD)
     suspend fun changePassword(@Path("id") id: Int, @Body request: ChangePasswordRequest): Response<Unit>
 
-    @GET(AppConstants.Api.Paths.CAREERS)
+    @GET(AppConstants.Api.Paths.ADMIN_CAREERS)
     suspend fun getCareers(): Response<List<Career>>
 
     @GET(AppConstants.Api.Paths.STUDENT_NOTES)
