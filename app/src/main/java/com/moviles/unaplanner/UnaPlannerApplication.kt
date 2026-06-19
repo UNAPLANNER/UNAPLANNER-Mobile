7438c24a51743e7c802b8f7cf3e59177835f67c9
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2026-06-19 17:55:20 -0600

    Firebase login and channel design fix

--- app/src/main/java/com/moviles/unaplanner/UnaPlannerApplication.kt
+++ app/src/main/java/com/moviles/unaplanner/UnaPlannerApplication.kt
@@ -2,10 +2,14 @@ package com.moviles.unaplanner
 
 import androidx.multidex.MultiDexApplication
 import com.moviles.unaplanner.data.AppContainer
+import com.moviles.unaplanner.notifications.NotificationHelper
 
 class UnaPlannerApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         AppContainer.init(this)
+        // Channel created here to exist even if the app is closed
+        // when a Firebase push arrives
+        NotificationHelper.createChannel(this)
     }
 }
